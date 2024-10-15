b466a38fef491a1293adf31766cb76e7241e7bdd
raroaslhey <raroaslheyjade@gmail.com>
2024-10-15 13:18:01 +0800

    Update ITCRARO_SEVENPM Sari-Sari Store Point of Sale System.cpp

--- ITCRARO_SEVENPM Sari-Sari Store Point of Sale System.cpp
+++ ITCRARO_SEVENPM Sari-Sari Store Point of Sale System.cpp
@@ -4,29 +4,57 @@ using namespace std;
 
 int main (){
 	string sugar, rice, sardines, coffee, milk;
-	float sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD, usdToPhp, poundToPhp, totalCostPhp;
-	int sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
+	float sugarPriceUSD, ricePricePound, sardinesPricePound, coffeePriceUSD, milkPriceUSD, usdToPhp, poundToPhp, totalCostPhp, totalCostPhpSR, totalCostPhpR, totalCostPhpSS, totalCostPhpC, totalCostPhpM;
+	int sugarQty, riceQty, sardinesQty, coffeeQty, milkQty, totalQty;
 	
-	usdToPhp= 1.00 * 57.28;
-	poundToPhp= 1.00 * 74.72;
-
+	usdToPhp = 1.00 * 57.28;
+	poundToPhp = 1.00 * 74.72;
 	
-	cout << "Item Name;\n";
-	cin >> sugar, rice, sardines, coffee, milk;
-	cout << "Quantity purchased:\n";
-	cin >> sugarQty, riceQty, sardinesQty, coffeeQty, milkQty;
-	sugarPriceUSD = sugarQty * usdToPhp;
-	ricePricePound = riceQty * poundToPhp;
-	sardinesPricePound = sardinesQty * poundToPhp;
-	coffeePriceUSD = coffeeQty * poundToPhp;
-	milkPriceUSD = milkQty * poundToPhp;
-	cout << "Price per item in PHP:\n" << sugarPriceUSD << endl;
-	totalCostPhp = sugarQty * usdToPhp;
-	totalCostPhp = riceQty * poundToPhp;
-	totalCostPhp = sardinesQty * poundToPhp;
-	totalCostPhp =coffeeQty * poundToPhp;
-	totalCostPhp = milkQty * poundToPhp;
-	cout << "Total cost per item in PHP:\n" << totalCostPhp << endl;
+	//list of items
+	cout << "LIST OF ITEMS: Sugar/Rice/Sardines/Coffee/Milk" << endl;
+	
+	//quantity of items
+	cout << "Sugar: ";
+	cin >> sugarQty;
+	cout << "Rice: ";
+	cin >> riceQty;
+	cout << "Sardines: ";
+	cin >> sardinesQty;
+	cout << "Coffe: ";
+	cin >> coffeeQty;
+	cout << "Milk: ";
+	cin >> milkQty;
+	//Total Quantities of items
+	totalQty = sugarQty + riceQty + sardinesQty + coffeeQty + milkQty;
+	cout << "Quantity purchased:\n" << totalQty << endl;
+	//Prices of items
+	cout << "Price per item in PHP:" << endl;
+	cout << "Sugar:$ ";
+	cin >> sugarPriceUSD;
+	cout << "Rice:P ";
+	cin >> ricePricePound;
+	cout << "Sardines:P ";
+	cin >> sardinesPricePound;
+	cout << "Coffe:P ";
+	cin >> coffeePriceUSD;
+	cout << "Milk:$ ";
+	cin >> milkPriceUSD;
+	//TOTAL COST PER ITEM
+	cout << "Total cost per item in PHP:" << endl;
+	totalCostPhpSR = sugarPriceUSD * usdToPhp;
+	cout << "Sugar:PHP " << totalCostPhpSR << endl;
+	totalCostPhpR = ricePricePound * poundToPhp; 
+	cout << "Rice:PHP " << totalCostPhpR << endl;
+	totalCostPhpSS = sardinesPricePound * poundToPhp;
+	cout << "Sardines:PHP " << totalCostPhpSS << endl;
+	totalCostPhpC = coffeePriceUSD * usdToPhp;
+	cout << "Coffe:PHP " << totalCostPhpC << endl;
+	totalCostPhpM = milkPriceUSD * usdToPhp;
+	cout << "Milk:PHP " << totalCostPhpM << endl;
+	//TOTAL COST OF ALL ITEM
+	totalCostPhp = totalCostPhpSR + totalCostPhpR + totalCostPhpSS + totalCostPhpC + totalCostPhpM;
+	cout << "TOTAL COST: " << totalCostPhp << endl;
+	
 	return 0;
 	
 } 
