6d0b881a6f80ff1a03331dd2948bcb1e34529692
raroaslhey <raroaslheyjade@gmail.com>
2024-11-06 12:16:02 +0800

    Add files via upload

---
+++ TEMPERATURE_SAMPLE.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	char choice;
+	
+	cout << "Ask user if it is raining [y/n] ";
+	cin >> choice;
+	
+	if (choice == 'y'){
+		cout << "Tell the user to get an umbrella";
+		
+	}else (choice == 'n');
+		cout << "Tell user it is sunny";
+
+	
+	return 0;
+
+}
