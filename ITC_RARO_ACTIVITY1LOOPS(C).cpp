1f5634fe987fc70cc14d5ea83eeb200f85241af4
raroaslhey <raroaslheyjade@gmail.com>
2024-11-29 23:35:39 +0800

    Update ITC_RARO_ACTIVITY1LOOPS(C).cpp

--- ITC_RARO_ACTIVITY1LOOPS(C).cpp
+++ ITC_RARO_ACTIVITY1LOOPS(C).cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main (){
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < i; j++) {
-			cout << "#";
+	for (int i = 1; i < 8; i++) {
+		for (int j = 1; j < i; j++) {
+			cout << " ";
 		}
-		for (int k = 0; k < 8; k++) {
-			
+		for (int j = 8; j > i; j--)  {
+		    cout << "#";
 		}
 		cout << endl;
 	}
